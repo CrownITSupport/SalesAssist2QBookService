c4535c70146ed74d99c4bb214f17c3c24ec236ba
CrownITSupport <ITSupport@crownmeat.com>
2020-11-06 04:02:07 -0800

    Just added Services and Services.Test projects

---
+++ SA2QBookSvc.Services.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SA2QBookSvc.Services.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SA2QBookSvc.Services.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("456d2c2c-fa28-49cf-8163-e71e42bb580c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SA2QBookSvc.Services/Class1.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+namespace SA2QBookSvc.Services {
+    public class Class1 {
+    }
+}
