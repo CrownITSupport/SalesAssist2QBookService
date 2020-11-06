760df67e67ed8e9cfa96cca7903aef7d6639e41d
CrownITSupport <ITSupport@crownmeat.com>
2020-11-06 08:55:07 -0800

    Created BDSPub and QBooks repos.  Unit tested open q QBSessionManager and connect to local quickbooks.

---
+++ SA2QBookSvc.DataAccessQuickBooks.Tests/RepositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SA2QBookSvc.DataAccessQuickBooks.Tests {
+    [TestClass]
+    public class RepositoryTests {
+        protected Repository _qbRepo = new Repository();
+        [TestMethod]
+        public void OpenConnection() {
+            // Arrange
+            // Act
+            var qbSession = _qbRepo.OpenConnection();
+            var msgSet = qbSession.CreateMsgSetRequest("US",13,0);
+            var objectToAdd = msgSet.AppendCurrencyQueryRq();
+            // Assert
+        }
+    }
+}
--- SA2QBookSvc.DataAccessQuickBooks/Repository.cs
+++ SA2QBookSvc.DataAccessQuickBooks/Repository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QBFC13Lib;
 
-namespace SA2QBookSvc.DataAccessQuickBooks
-{
-    public class Repository
-    {
+namespace SA2QBookSvc.DataAccessQuickBooks {
+    public class Repository {
+        public QBSessionManager OpenConnection() {
+            /* 11/6/20 8:42 Pwd: Duk3Ha$ */
+            QBSessionManager qbData = new QBSessionManager();
+            qbData.OpenConnection("","SA2QBook");
+            //qbData.OpenConnection(AppID,AppName)
+            //qbData.BeginSession(qbFile,openMode)
+            //qbData.BeginSession("Crown Meat Prototype 2_E2",ENOpenMode.omDontCare);
+            qbData.BeginSession("",ENOpenMode.omDontCare);
+            return qbData;
+        }
     }
 }
